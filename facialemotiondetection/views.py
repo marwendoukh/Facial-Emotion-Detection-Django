ce1439d60d1ba8463849e48b139ee1e9a643ab8e
Marwen Doukh <marwen.doukh@gmail.com>
2017-04-16 15:37:36 +0100

    improve index function

--- facialemotiondetection/views.py
+++ facialemotiondetection/views.py
@@ -5,7 +5,6 @@ import cv2
 def index(request):
   while (True):
 
-    imagePath = "/home/marwen/Downloads/djangoTesting/Django_face_recognition/website/mysite/opencv/passenger2.jpg"
     cascPath = "/home/marwen/Downloads/djangoTesting/Django_face_recognition/website/mysite/opencv/haarcascade_frontalface_default.xml"
     i = 0
     # Create the haar cascade
@@ -15,9 +14,7 @@ def index(request):
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    # Read the image
-    image = cv2.imread(imagePath,1)
-    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+   
     # Detect faces in the image
     faces = faceCascade.detectMultiScale(
         frame,
@@ -28,6 +25,5 @@ def index(request):
     )
 
     facesNumber ="Found {0} faces!".format(len(faces))
-    cv2.imwrite("/home/marwen/Downloads/djangoTesting/Django_face_recognition/website/mysite/opencv/passenger_facesDDDDDD.jpg", frame)
 
     return HttpResponse(facesNumber)
